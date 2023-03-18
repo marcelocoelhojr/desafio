4d56adf754fd18ff94d215e988e6f4655474a077
Marcelo Coelho <macj.10@hotmail.com>
2023-03-17 22:02:10 -0300

    create addresses and jobs migrations

---
+++ database/migrations/2023_03_18_003744_create_addresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('state');
+            $table->string('city', 120);
+            $table->string('cep')->nullable();
+            $table->string('neighborhood', 120)->nullable();
+            $table->string('street', 120)->nullable();
+            $table->integer('number')->nullable();
+            $table->string('complement', 250)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
---
+++ database/migrations/2023_03_18_004358_create_job_vacancies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_vacancies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250);
+            $table->enum('modality', ['Freelancer', 'PJ', 'CLT']);
+            $table->enum('type', ['Presencial', 'Remoto', 'Hibrido']);
+            $table->double('salary', 5.3)->nullable();
+            $table->string('image');
+            $table->string('description', 250);
+            $table->unsignedBigInteger('address_id');
+            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_vacancies');
+    }
+};
